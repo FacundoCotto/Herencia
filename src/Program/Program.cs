9cb7820af3fc986291f08c8be11e500bec767026
FacundoCotto <facucottoalvarez@email.com>
2026-09-24 09:02:49 -0300

    Segundo modelo en preparacion

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -4,9 +4,7 @@
 // </copyright>
 //------------------------------------------------------------------------------
 
-using System;
 using System.Threading.Tasks;
-using Ucu.Poo.Discord;
 
 namespace Ucu.Poo.RideShare
 {
@@ -27,58 +25,13 @@ namespace Ucu.Poo.RideShare
 
         private static async Task MainAsync()
         {
-            var botToken = Environment.GetEnvironmentVariable("DISCORD_BOT_TOKEN");
-            var channelText = Environment.GetEnvironmentVariable("CHANNEL_ID");
-            if (string.IsNullOrWhiteSpace(botToken) || string.IsNullOrWhiteSpace(channelText))
-            {
-                Console.WriteLine("Faltan las variables de entorno.");
-                return;
-            }
 
-            ulong channelId = ulong.Parse(channelText);
-
-            DiscordClient discord = new DiscordClient();
-
-            /*
-            Mira a continuación cómo enviar mensajes y archivos a Discord;
-            deberás hacer lo mismo en las clases y métodos adecuados de tu
-            solución. Luego puedes comentar o eliminar este código.
-            */
-
-            Console.WriteLine("Conectando con Discord...");
-            await discord.LoginAsync(botToken);
-            await discord.SendMessageAsync(channelId, "¡Hola desde C#!");
-            await discord.SendImageAsync(channelId, "bill.jpg", "Mira esta imagen");
-            Console.WriteLine("Mensajes enviados.");
-
-            /*
-            En este método deberás mostrar un ejemplo de funcionamiento de tu
-            solución. A continuación te planteamos un ejemplo de como hacerlo.
-            Esto no significa que te limites a hacer solamente esto, ¡debes
-            pensar en grande!
-
-            User pasajero1 = ...
-            User pasajero2 = ...
-            User pasajero3 = ...
-            User conductor1 = ...
-            User conductorPool1 = ...
-            UcuRideShare rideShare = new UcuRideShare()
-
-            rideShare.Add(conductor1)
-            Se publica en Discord un nuevo conductor!
-
-            rideShare.Add(conductorPool1)
-            Se publica en Discord un nuevo conductor!
-
-            rideShare.Add(pasajero1)
-            Se publica en Discord nuevo registro de pasajero!
-
-            rideShare.Add(pasajero2)
-            Se publica en Discord nuevo registro de pasajero!
-
-            rideShare.Add(pasajero3)
-            Se publica en Discord nuevo registro de pasajero!
-            */
+            Register register = new Register(new DiscordPrinter());
+            User passenger1 = register.RegisterPassenger("Bill", "Gates", "1234567-8", "/biil.jpg", 5);
+            User passenger2 = register.RegisterPassenger("Dan", "Brown", "2345678-9", "/dan.jpg", 4);
+            User passenger3 = register.RegisterPassenger("Rick", "Sanchez", "3456789-0", "/rick.jpg", 3);
+            User driver = register.RegisterDriver("Rick", "Grimes", "4567890-1", "/rick.jpg",
+                "Manejo desde hace 10 años", 5, new Car("Toyota Corolla", 2018));
         }
     }
 }
